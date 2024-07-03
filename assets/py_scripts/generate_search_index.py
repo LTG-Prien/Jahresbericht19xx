f86cb214476b57fc6811ace19087f2e0b3066794
L3g7 <contact@l3g7.dev>
2024-07-03 20:49:23 +0200

    Fix search index generation

--- assets/py_scripts/generate_search_index.py
+++ assets/py_scripts/generate_search_index.py
@@ -5,6 +5,9 @@ ARCHIVE_PATH = "../../archiv"
 
 
 def sanitize(s: str):
+    while s.strip().startswith("<"):
+        s = s[s.index(">") + 1:]
+
     escaped_chars = re.findall(r"&#(\d+);", s)
     for char in escaped_chars:
         if int(char) == 134:
@@ -14,7 +17,26 @@ def sanitize(s: str):
 
     s = re.sub(r"\s\s+", " ", s)
     s = re.sub(r"^\d+[.,]\s*", "", s)
-    return s.strip()
+    s = s.replace("&uuml;", "ü").replace("&auml;", "ä").replace("&ouml;", "ö").replace("&Ouml;", "Ö")
+    s = s.replace("&szlig;", "ß")
+    s = s.replace("&eacute;", "é")
+    s = s.strip()
+    return s
+
+    escaped_chars = re.findall(r"&#(\d+);", s)
+    for char in escaped_chars:
+        if int(char) == 134:
+            s = s.replace(f"&#{char};", "")
+        else:
+            s = s.replace(f"&#{char};", chr(int(char)))
+
+    s = re.sub(r"\s\s+", " ", s)
+    s = re.sub(r"^\d+[.,]\s*", "", s)
+    s = s.replace("&uuml;", "ü").replace("&auml;", "ä").replace("&ouml;", "ö").replace("&Ouml;", "Ö")
+    s = s.replace("&szlig;", "ß")
+    s = s.replace("&eacute;", "é")
+    s = s.strip()
+    return s
 
 
 index = {}
@@ -28,13 +50,14 @@ def process(years, class_years, prefix):
             if not os.path.isfile(path):
                 continue
 
-            parts = open(path).read().split("<h3")[1:]
+            parts = re.split("<h3", open(path).read(), flags=re.IGNORECASE)[1:]
             for content in parts:
                 title = re.findall(r"[^>]*>\s*([^<\r\n(]+)", content)[0]
                 title = re.sub(r"klasse:?|kollegstufe| |m?ng", "", title.lower())
 
-                names = re.findall(r"<tr>\s*<td[^>]*>([^<]+)", content)
+                names = re.findall(r'<(?:tr|TR)>\s*<(?:td|TD)(?![^>]* class="foto">)[^>]*>([^<]+(?:<FONT[^>]+>\s*)?(?:<P>[^<]+)?)', content)
                 for name in names:
+                    x = name
                     name = sanitize(name)
                     if name == "":
                         continue
@@ -45,6 +68,8 @@ def process(years, class_years, prefix):
 
 process((53, 65), (1, 10), "0")
 process((65, 99), (5, 14), "")
+
+print("Found", len(index), "people.")
 
 f = open(SEARCH_INDEX_PATH, "w", encoding="utf-8")
 f.write("// Automatisch generiert.\n")
