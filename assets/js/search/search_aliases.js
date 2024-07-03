e697f5d57659c1a22dec161cde0e9e30e3598a48
L3g7 <contact@l3g7.dev>
2024-07-03 20:54:01 +0200

    Update search aliases

--- assets/js/search/search_aliases.js
+++ assets/js/search/search_aliases.js
@@ -2,5 +2,7 @@
 // (Wegen kleinen Abweichungen durch OCR-Fehlern, z.B. rn/m; FI/A)
 const search_aliases = {
     "Moosbauer": "Moosbauer Christine", // Waren in 2 Zeilen aufgeteilt: Moosbauer | Prien\nChristine | Prien
-    "Kraska": "Kraska Frank"
+    "Kraska": "Kraska Frank", // Fehlt komplett der Vorname
+
+    "Peters Kerstin-Elisabeth": "Peters Kerstin"
 };
